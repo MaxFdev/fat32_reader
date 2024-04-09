b5827dc407ceb361940bcc97631d9ee8603f6aaa
m <yuvm>
2024-04-08 21:02:02 -0400

    Refactor fat32_reader class to improve file handling and error checking

--- fat32_reader.java
+++ fat32_reader.java
@@ -1,22 +1,44 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class fat32_reader {
 
+    private static boolean fileExists = true;
+
     public static void main(String[] args) {
+        // check for one arg
         if (args.length != 1) {
-            throw new IllegalArgumentException("Requires 1 arg, ie: fat32_reader <fat32.img>");
-        } else {
-            fat32_reader fsReader = new fat32_reader(args[0]);
-            fsReader.start();
+            throw new IllegalArgumentException("Requires 1 arg, ie: java fat32_reader <fat32.img>");
         }
+
+        // make a file reader
+        fat32_reader fsReader = new fat32_reader(args[0]);
+
+        // check that the file exists
+        if (!fileExists) {
+            throw new IllegalArgumentException("Arg: <" + args[0] + "> not valid file name!");
+        }
+
+        // start the file reader
+        fsReader.start();
     }
 
-    private final String fileName;
+    private final File fat32;
 
     private fat32_reader(String fileName) {
-        this.fileName = fileName;
+        // open the file
+        this.fat32 = new File(fileName);
 
+        // check that the file exists
+        if (!this.fat32.exists()) {
+            fat32_reader.fileExists = false; // TODO does this work?
+        }
+
+        // call to get the file system specifications
+        scanSpecs();
     }
+
+    private void scanSpecs() {}
 
     private void start() {}
 }
---
+++ mntfat32.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mount -o loop fat32.img /mnt/fat32
