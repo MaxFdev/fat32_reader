d9675a4552e460dd26d78703c02192cf7fd08195
m <>
2024-04-19 13:46:46 -0400

    Scan fire file system specs.

--- fat32_reader.java
+++ fat32_reader.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
 
@@ -33,14 +34,34 @@ public class fat32_reader {
 
         // get the file system specs
         saveSpecs();
-
-        // start scanning input
-        scanInput();
     }
 
     // ! file system specifications
 
-    private static void saveSpecs() {}
+    private static int BPB_BytesPerSec;
+    private static int BPB_SecPerClus;
+    private static int BPB_RsvdSecCnt;
+    private static int BPB_NumFATS;
+    private static int BPB_FATSz32;
+
+    private static void saveSpecs() {
+        try {
+            fs.seek(11);
+            BPB_BytesPerSec = Short.reverseBytes(fs.readShort());
+            BPB_SecPerClus = fs.read();
+            fs.seek(14);
+            BPB_RsvdSecCnt = Short.reverseBytes(fs.readShort());
+            BPB_NumFATS = fs.read();
+            fs.seek(36);
+            BPB_FATSz32 = Integer.reverseBytes(fs.readInt());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // start scanning input
+        scanInput();
+    }
 
     // ! take commands input
 
