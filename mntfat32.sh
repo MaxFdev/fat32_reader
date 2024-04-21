133408b105780afb9874c361f4eeeff8d472af5e
m <>
2024-04-21 16:34:59 -0400

    Updated mounting script and added unmounting script

--- mntfat32.sh
+++ mntfat32.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+if [ -d "/mnt/fat32" ]; then
+    echo "Directory /mnt/fat32 already exists."
+else
+    echo "Directory /mnt/fat32 created."
+    mkdir /mnt/fat32
+fi
+
 mount -o loop fat32.img /mnt/fat32
+echo "File system 'fat32' mounted at /mnt/fat32."
---
+++ unmount.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+umount /mnt/fat32
+echo "File system 'fat32' unmounted from /mnt/fat32."
