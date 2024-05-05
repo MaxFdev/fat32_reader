50bb11f3e2ddd8fd985664942e44d06dac1b1305
m <>
2024-05-05 14:28:08 -0400

    got ls to work in root

--- fat32_reader.java
+++ fat32_reader.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class fat32_reader {
@@ -51,6 +54,7 @@ public class fat32_reader {
         try {
             // seek and read data
             fs.seek(11);
+            // TODO switch to custom byte reading method
             BPB_BytesPerSec = Short.reverseBytes(fs.readShort());
             BPB_SecPerClus = fs.read();
             fs.seek(14);
@@ -85,18 +89,15 @@ public class fat32_reader {
 
             // set the working directory to root
             workingDirectory = "/";
-    
+
             // navigating to root cluster (set the current cluster)
             dataRegOffset = (BPB_RsvdSecCnt + BPB_NumFATS * BPB_FATSz32) * BPB_BytesPerSec;
-            currentLocationByte = (BPB_RootClus - 2) * (BPB_SecPerClus * BPB_BytesPerSec) + dataRegOffset;
+            currentLocationByte = (BPB_RootClus - 2) * (BPB_SecPerClus * BPB_BytesPerSec)
+                    + dataRegOffset;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    // ! navigate to cluster
-
-    // TODO navigate to cluster method
 
     // ! take commands input
 
@@ -115,6 +116,7 @@ public class fat32_reader {
                 printInfo();
             } else if (input.equals("ls")) {
                 listDirectory();
+                // TODO make sure all the following are case insensitive
             } else if (input.startsWith("stat ")) {
                 // TODO does the substring work?
                 getStats(input.substring(input.indexOf(" ") + 1));
@@ -152,7 +154,103 @@ public class fat32_reader {
     // ! list directory
 
     private static void listDirectory() {
-        // TODO list directory
+        // get the entries
+        List<String> entries = getEntries();
+
+        // print the entries
+        for (int i = 0; i < entries.size(); i++) {
+            // print the entry
+            System.out.print(entries.get(i));
+
+            // print a space if not the last entry
+            if (i < entries.size() - 1) {
+                System.out.print(" ");
+            } else {
+                System.out.println();
+            }
+        }
+    }
+
+    private static List<String> getEntries() {
+        // make a list for all the entries
+        List<String> entries = new ArrayList<String>();
+
+        // add the current and parent directory
+        entries.add(".");
+        entries.add("..");
+
+        // read the entries
+        try {
+            // get the starting byte
+            fs.seek(currentLocationByte);
+            int startByte = fs.read();
+
+            // get the attribute byte
+            fs.seek(currentLocationByte + 11);
+            int attributeByte = fs.read();
+
+            // keep track of the current entry being looked at
+            int entry = 0;
+
+            // TODO also check if the end of the cluster is reached (if so check if there is
+            // another cluster to go to)
+            while (startByte != 0) {
+                if (!(startByte == 65 && attributeByte == 15) && startByte != 229
+                        && attributeByte != 8) {
+                    // build the entry name
+                    StringBuilder entryName = new StringBuilder();
+
+                    // read the name
+                    for (int i = 0; i < 8; i++) {
+                        fs.seek(currentLocationByte + 32 * entry + i);
+                        char nameChar = (char) fs.read();
+                        if (isPrintableChar(nameChar) && nameChar != ' ') {
+                            entryName.append(nameChar);
+                        }
+                    }
+
+                    // read the extension
+                    fs.seek(currentLocationByte + 32 * entry + 8);
+                    char extensionChar = (char) fs.read();
+
+                    // check if the extension is valid
+                    if (isPrintableChar(extensionChar) && extensionChar != ' ') {
+                        entryName.append(".");
+                        entryName.append(extensionChar);
+
+                        // finish the extension
+                        for (int i = 1; i < 3; i++) {
+                            extensionChar = (char) fs.read();
+                            if (isPrintableChar(extensionChar) && extensionChar != ' ') {
+                                entryName.append(extensionChar);
+                            }
+                        }
+                    }
+
+                    // add the entry to the list
+                    entries.add(entryName.toString());
+                }
+
+                // increment the entry
+                entry++;
+
+                // get the next starting byte
+                fs.seek(currentLocationByte + 32 * entry);
+                startByte = fs.read();
+
+                // get the next attribute byte
+                fs.seek(currentLocationByte + 32 * entry + 11);
+                attributeByte = fs.read();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // sort the entries for printing
+        Collections.sort(entries);
+
+        return entries;
     }
 
     // ! get file or directory stats
@@ -177,5 +275,11 @@ public class fat32_reader {
 
     private static void readFile(String args) {
         // TODO read file
+    }
+
+    // ! helper methods
+
+    private static boolean isPrintableChar(char c) {
+        return c >= 32 && c < 127;
     }
 }
