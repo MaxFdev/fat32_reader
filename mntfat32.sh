58c7369349e344f35425f9aeb99982b3a9697d19
m <>
2024-05-05 11:28:02 -0400

    Fixed scripts

--- mntfat32.sh
+++ mntfat32.sh
@@ -3,9 +3,9 @@
 if [ -d "/mnt/fat32" ]; then
     echo "Directory /mnt/fat32 already exists."
 else
+    mkdir /mnt/fat32 || exit 1
     echo "Directory /mnt/fat32 created."
-    mkdir /mnt/fat32
 fi
 
-mount -o loop fat32.img /mnt/fat32
+mount -o loop fat32.img /mnt/fat32 || exit 1
 echo "File system 'fat32' mounted at /mnt/fat32."
--- unmount.sh
+++ unmount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-umount /mnt/fat32
+umount /mnt/fat32 || exit 1
 echo "File system 'fat32' unmounted from /mnt/fat32."
